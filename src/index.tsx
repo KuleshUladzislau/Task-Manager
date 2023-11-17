b9e90691ab9e3ffc2c7a0d5196ea5bc6a7d4fc5d
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-11-17 14:22:02 +0300

    added hash

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 import { store} from "./redux/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
 
