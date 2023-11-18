e8caa1484572197eb25c7058f63577e46b67ed02
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-11-18 18:04:58 +0300

    predeploy

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 import { store} from "./redux/store";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App/>
-        </HashRouter>
+        </BrowserRouter>
     </Provider>
 );
 
